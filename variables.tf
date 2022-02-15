a2255ae82d32bb1af47475e82578ecf22380cd5b
Brett Bergin <brettberginbc@yahoo.com>
2022-02-14 18:38:09 -0800

    Initial Commt

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "instance_count" {
+  default = "2"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "main_vpc_cidr" {
+  default = "100.64.0.0/16"
+}
+
+variable "public_subnets" {
+  default = "100.64.128.0/24"
+}
+
+variable "private_subnets" {
+  default = "100.64.64.0/24"
+}
